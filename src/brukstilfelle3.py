bbdf08134fa10e1d7a64a286f7b14f85f2df2e8d
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-03-15 23:27:27 +0100

    har funnet ledig rad. Skal sette inn i tabell

---
+++ src/brukstilfelle3.py
@@ -0,0 +1,37 @@
+from connection import con
+import datetime
+
+cursor = con.cursor()
+
+cursor.execute(
+"""SELECT S.Område, S.RadNr, COUNT(*) AS AntallLedige
+FROM Sete AS S
+WHERE (Område, RadNr, SeteNr) NOT IN    (SELECT Område, RadNr, SeteNr
+                                        FROM ((BillettOmråde NATURAL JOIN BillettRadNr)
+                                        NATURAL JOIN BillettSete)
+                                        NATULAR JOIN BillettKjøpForestilling
+                                        WHERE Tid = 18.30)
+GROUP BY Område, RadNr""")
+
+ret = cursor.fetchall()
+print(ret)
+
+dt = datetime.now()
+
+if ret:
+    valgtRad = ret[0]
+    cursor.execute(
+"""INERT INTO BillettKjøpKunde
+VALUES (3, 1)"""
+)
+    cursor.execute(
+"""INSERT INTO BillettKjøpsDato
+VALUES (3, ?, ?)""",
+(f'{dt.year}-{dt.month}-{dt.day}', f'{dt.hour}:{dt.minute}')
+)
+    cursor.execute(
+"""INSERT INTO BillettKjøpKunde
+VALUES (3,1)"""
+)
+
+cursor.close()
