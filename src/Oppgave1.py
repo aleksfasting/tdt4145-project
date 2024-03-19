e6cfa8f4be87d3bade53f63a070f29415bc99fd6
Ulrik Haugland <ulhaugland@gmail.com>
2024-03-19 11:22:46 +0100

    Ny underveisoppdatering.

--- src/Oppgave1.py
+++ src/Oppgave1.py
@@ -40,10 +40,12 @@ def SettInnSete():
 
 def SettInnAkt():
     
-    con.execute("INSERT INTO Sal VALUES (1, 1, NULL)")
-    con.execute("INSERT INTO Sal VALUES (1, 2, NULL)")
-    con.execute("INSERT INTO Sal VALUES (1, 3, NULL)")
-    con.execute("INSERT INTO Sal VALUES (2, 1, NULL)")
+    con.execute("INSERT INTO Akt VALUES (1, 1, NULL)")
+    con.execute("INSERT INTO Akt VALUES (1, 2, NULL)")
+    con.execute("INSERT INTO Akt VALUES (1, 3, NULL)")
+    con.execute("INSERT INTO Akt VALUES (1, 4, NULL)")
+    con.execute("INSERT INTO Akt VALUES (1, 5, NULL)")
+    con.execute("INSERT INTO Akt VALUES (2, 1, NULL)")
     
     con.commit()
     con.close()
@@ -80,76 +82,131 @@ def SettInnRolle():
 def SettInnPerson():
     
     con.execute("INSERT INTO Person VALUES (1,'Arturo Scotti','arsc@gmail.com', 'Freelance')")
-    con.execute("INSERT INTO Person VALUES (2,'Ingunn Beate Strige Øyen', 'inøy@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (3,'Hans Petter Nilsen', 'hani@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (4,'Madeleine Brandtzæg', 'mabr@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (5,'Synnøve Fossum Eriksen', 'syer@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (6,'Emma Caroline Deichmann', 'emde@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (7,'Thomas Jensen Takyi', 'thta@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (8,'Per Bogstad Gulliksen', 'pegu@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (9, 'Isak Holmen Sørensen', 'issø@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (10,'Fabian Heidelberg Lunde', 'falu@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (11,'Emil Olafsson', 'emol@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (12,'Snorre Ryen Tøndel', 'sntø@gmail.com', 'Freelance)")
+    con.execute("INSERT INTO Person VALUES (2,'Ingunn Beate Strige Øyen', 'inøy@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (3,'Hans Petter Nilsen', 'hani@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (4,'Madeleine Brandtzæg', 'mabr@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (5,'Synnøve Fossum Eriksen', 'syer@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (6,'Emma Caroline Deichmann', 'emde@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (7,'Thomas Jensen Takyi', 'thta@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (8,'Per Bogstad Gulliksen', 'pegu@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (9, 'Isak Holmen Sørensen', 'issø@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (10,'Fabian Heidelberg Lunde', 'falu@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (11,'Emil Olafsson', 'emol@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (12,'Snorre Ryen Tøndel', 'sntø@gmail.com', 'Freelance')")
 
-    con.execute("INSERT INTO Person VALUES (13,'Yury Butusov','yubu@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (14,'Aleksandr Shishkin-Hokusai','alsh@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (15,'Eivind Myren','eimy@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (16,'Mina Rype Stokke','mist@gmail.com', 'Freelance)")
+    con.execute("INSERT INTO Person VALUES (13,'Yury Butusov','yubu@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (14,'Aleksandr Shishkin-Hokusai','alsh@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (15,'Eivind Myren','eimy@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (16,'Mina Rype Stokke','mist@gmail.com', 'Freelance')")
 
-    con.execute("INSERT INTO Person VALUES (17,'Sunniva Du Mond Nordal','suno@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (18,'Jo Sabiernak','josa@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (19,'Marte M. Steinsholt', 'mast@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (20,'Tor Ivar Hagen', 'toha@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (21,'Trond-Ove Skrødal','trsk@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (22,'Natalie Grøndahl Tangen', 'nata@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (23,'Åsmund Flaten', 'åsfl@gmail.com', 'Freelance)")
+    con.execute("INSERT INTO Person VALUES (17,'Sunniva Du Mond Nordal','suno@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (18,'Jo Sabiernak','josa@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (19,'Marte M. Steinsholt', 'mast@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (20,'Tor Ivar Hagen', 'toha@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (21,'Trond-Ove Skrødal','trsk@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (22,'Natalie Grøndahl Tangen', 'nata@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (23,'Åsmund Flaten', 'åsfl@gmail.com', 'Freelance')")
     
-    con.execute("INSERT INTO Person VALUES (24,'Jonas Corell Petersen','jope@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (25,'David Gehrt', 'dage@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (26,'Gaute Tønder','gatø@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (27,'Magnus Mikaelsen', 'mami@gmail.com', 'Freelance)")
-    con.execute("INSERT INTO Person VALUES (28,'Kristoffer Spender', 'krsp@gmail.com', 'Freelance)")
+    con.execute("INSERT INTO Person VALUES (24,'Jonas Corell Petersen','jope@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (25,'David Gehrt', 'dage@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (26,'Gaute Tønder','gatø@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (27,'Magnus Mikaelsen', 'mami@gmail.com', 'Freelance')")
+    con.execute("INSERT INTO Person VALUES (28,'Kristoffer Spender', 'krsp@gmail.com', 'Freelance')")
 
     con.commit()
     con.close()
 
 def SettInnSkuespillere():
-    con.execute("INSERT INTO Person VALUES (1)")
-    con.execute("INSERT INTO Person VALUES (2)")
-    con.execute("INSERT INTO Person VALUES (3)")
-    con.execute("INSERT INTO Person VALUES (4)")
-    con.execute("INSERT INTO Person VALUES (5)")
-    con.execute("INSERT INTO Person VALUES (6)")
-    con.execute("INSERT INTO Person VALUES (7)")
-    con.execute("INSERT INTO Person VALUES (8)")
-    con.execute("INSERT INTO Person VALUES (9)")
-    con.execute("INSERT INTO Person VALUES (10)")
-    con.execute("INSERT INTO Person VALUES (11)")
-    con.execute("INSERT INTO Person VALUES (12)")
+    con.execute("INSERT INTO Skuespiller VALUES (1)")
+    con.execute("INSERT INTO Skuespiller VALUES (2)")
+    con.execute("INSERT INTO Skuespiller VALUES (3)")
+    con.execute("INSERT INTO Skuespiller VALUES (4)")
+    con.execute("INSERT INTO Skuespiller VALUES (5)")
+    con.execute("INSERT INTO Skuespiller VALUES (6)")
+    con.execute("INSERT INTO Skuespiller VALUES (7)")
+    con.execute("INSERT INTO Skuespiller VALUES (8)")
+    con.execute("INSERT INTO Skuespiller VALUES (9)")
+    con.execute("INSERT INTO Skuespiller VALUES (10)")
+    con.execute("INSERT INTO Skuespiller VALUES (11)")
+    con.execute("INSERT INTO Skuespiller VALUES (12)")
 
-    con.execute("INSERT INTO Person VALUES (17)")
-    con.execute("INSERT INTO Person VALUES (18)")
-    con.execute("INSERT INTO Person VALUES (19)")
-    con.execute("INSERT INTO Person VALUES (20)")
-    con.execute("INSERT INTO Person VALUES (21)")
-    con.execute("INSERT INTO Person VALUES (22)")
-    con.execute("INSERT INTO Person VALUES (23)")
+    con.execute("INSERT INTO Skuespiller VALUES (17)")
+    con.execute("INSERT INTO Skuespiller VALUES (18)")
+    con.execute("INSERT INTO Skuespiller VALUES (19)")
+    con.execute("INSERT INTO Skuespiller VALUES (20)")
+    con.execute("INSERT INTO Skuespiller VALUES (21)")
+    con.execute("INSERT INTO Skuespiller VALUES (22)")
+    con.execute("INSERT INTO Skuespiller VALUES (23)")
     
     
 def SettInnMedvirkende():
-    con.execute("INSERT INTO Person VALUES (13)")
-    con.execute("INSERT INTO Person VALUES (14)")
-    con.execute("INSERT INTO Person VALUES (15)")
-    con.execute("INSERT INTO Person VALUES (16)")
+    con.execute("INSERT INTO Personell VALUES (13)")
+    con.execute("INSERT INTO Personell VALUES (14)")
+    con.execute("INSERT INTO Personell VALUES (15)")
+    con.execute("INSERT INTO Personell VALUES (16)")
 
-    con.execute("INSERT INTO Person VALUES (24)")
-    con.execute("INSERT INTO Person VALUES (25)")
-    con.execute("INSERT INTO Person VALUES (26)")
-    con.execute("INSERT INTO Person VALUES (27)")
-    con.execute("INSERT INTO Person VALUES (28)")
-
-def SettInnStoler():
+    con.execute("INSERT INTO Personell VALUES (24)")
+    con.execute("INSERT INTO Personell VALUES (25)")
+    con.execute("INSERT INTO Personell VALUES (26)")
+    con.execute("INSERT INTO Personell VALUES (27)")
+    con.execute("INSERT INTO Personell VALUES (28)")
     
+
+def SettInnRelasjoner():
+    #kongsemnene
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 1, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 1, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 1, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 1, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 1, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 2, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 2, 3)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 3, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 3, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 3, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 3, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 3, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 4, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 4, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 5, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 5, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 5, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 5, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 5, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 6, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 6, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 6, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 7, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 7, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 7, 3)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 8, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 8, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 8, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 8, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 8, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 9, 1)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 9, 2)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 9, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 9, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 9, 5)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 10, 1)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 11, 4)")
+
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 12, 3)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 12, 4)")
+    con.execute("INSERT INTO RolleIAkt VALUES(1, 12, 5)")
+
+
+
 
 
---
+++ src/Oppgave1.sql
@@ -0,0 +1,171 @@
+INSERT INTO Forestilling(`Dato`, `Tid`) VALUES 
+    ('2024-02-01','19:00'),
+    ('2024-02-02','19:00'),
+    ('2024-02-03','19:00'),
+    ('2024-02-05','19:00'),
+    ('2024-02-06','19:00'),
+    ('2024-02-03','18:30'),
+    ('2024-02-06','18:30'),
+    ('2024-02-07','18:30'),
+    ('2024-02-12','18:30'),
+    ('2024-02-13','18:30'),
+    ('2024-02-14','18:30');
+
+INSERT INTO TeaterStykke(`SID`, `Navn`, `Forfatter`, `Tid`) VALUES 
+    (1, 'Kongsemnene', 'Henrik Ibsen', 19.00),
+    (2, 'Størst av alt er kjærligheten', 'Jonas Corell Petersen', 18.30);
+
+INSERT INTO Sal(`SalNavn`, `SID`) VALUES 
+    ('Hovedscenen', 1),
+    ('Gamle Scene', 2);
+
+INSERT INTO Akt(`SID`, `AktNr`, `AktNavn`) VALUES 
+    (1, 1, NULL),
+    (1, 2, NULL),
+    (1, 3, NULL),
+    (1, 4, NULL),
+    (1, 5, NULL),
+    (2, 1, NULL);
+
+INSERT INTO Rolle(`SID`, `RolleNavn`) VALUES 
+    (1,'Haakon Haakonssønn'),
+    (1,'Inga fra Vartejg'),
+    (1,'Skule Jarl'),
+    (1, 'Fru Ragnhild'),
+    (1, 'Margrete'),
+    (1, 'Sigrid/Ingebjørg'),
+    (1, 'Biskop Nikolas'),
+    (1, 'Gregorius Jonssønn'),
+    (1, 'Paal Flida/Trønder'),
+    (1, 'Baard Bratte/Trønder'),
+    (1, 'Jatgeir Skald/Dagfinn Bonde'),
+    (1, 'Peter'),
+    (2,'Sunniva Du Mond Nordal'),
+    (2,'Jo Sabiernak'),
+    (2,'Marte M. Steinsholt'),
+    (2,'Tor Ivar Hagen'),
+    (2,'Trond-Ove Skrødal'),
+    (2,'Natalie Grøndahl Tangen'),
+    (2,'Åsmund Flaten');
+
+INSERT INTO Person(`PID`,`Navn`,`Epost`,`Status`) VALUES 
+    (1,'Arturo Scotti','arsc@gmail.com', 'Freelance'),
+    (2,'Ingunn Beate Strige Øyen', 'inøy@gmail.com', 'Freelance'),
+    (3,'Hans Petter Nilsen', 'hani@gmail.com', 'Freelance'),
+    (4,'Madeleine Brandtzæg', 'mabr@gmail.com', 'Freelance'),
+    (5,'Synnøve Fossum Eriksen', 'syer@gmail.com', 'Freelance'),
+    (6,'Emma Caroline Deichmann', 'emde@gmail.com', 'Freelance'),
+    (7,'Thomas Jensen Takyi', 'thta@gmail.com', 'Freelance'),
+    (8,'Per Bogstad Gulliksen', 'pegu@gmail.com', 'Freelance'),
+    (9, 'Isak Holmen Sørensen', 'issø@gmail.com', 'Freelance'),
+    (10,'Fabian Heidelberg Lunde', 'falu@gmail.com', 'Freelance')
+    (11,'Emil Olafsson', 'emol@gmail.com', 'Freelance'),
+    (12,'Snorre Ryen Tøndel', 'sntø@gmail.com', 'Freelance'),
+    (13,'Yury Butusov','yubu@gmail.com', 'Freelance'),
+    (14,'Aleksandr Shishkin-Hokusai','alsh@gmail.com', 'Freelance'),
+    (15,'Eivind Myren','eimy@gmail.com', 'Freelance'),
+    (16,'Mina Rype Stokke','mist@gmail.com', 'Freelance'),
+    (17,'Sunniva Du Mond Nordal','suno@gmail.com', 'Freelance'),
+    (18,'Jo Sabiernak','josa@gmail.com', 'Freelance'),
+    (19,'Marte M. Steinsholt', 'mast@gmail.com', 'Freelance'),
+    (20,'Tor Ivar Hagen', 'toha@gmail.com', 'Freelance'),
+    (21,'Trond-Ove Skrødal','trsk@gmail.com', 'Freelance'),
+    (22,'Natalie Grøndahl Tangen', 'nata@gmail.com', 'Freelance'),
+    (23,'Åsmund Flaten', 'åsfl@gmail.com', 'Freelance'),
+    (24,'Jonas Corell Petersen','jope@gmail.com', 'Freelance'),
+    (25,'David Gehrt', 'dage@gmail.com', 'Freelance'),
+    (26,'Gaute Tønder','gatø@gmail.com', 'Freelance'),
+    (27,'Magnus Mikaelsen', 'mami@gmail.com', 'Freelance'),
+    (28,'Kristoffer Spender', 'krsp@gmail.com', 'Freelance')
+    ON CONFLICT (`Epost`) DO NOTHING;
+
+INSERT INTO Skuespiller(`PID`) VALUES 
+    (1),
+    (2),
+    (3),
+    (4),
+    (5),
+    (6),
+    (7),
+    (8),
+    (9),
+    (10),
+    (11),
+    (12),
+    (17),
+    (18),
+    (19),
+    (20),
+    (21),
+    (22),
+    (23);
+
+INSERT INTO personell(`PID`) VALUES
+    (13),
+    (14),
+    (15),
+    (16),
+    (24),
+    (25),
+    (26),
+    (27),
+    (28);
+
+ #Kongsemnene
+INSERT INTO RolleIAkt(`SID`,`RolleNavn`,`AktNr`) VALUES
+    (1, 'Haakon Haakonssønn', 1),
+    (1, 'Haakon Haakonssønn', 2),
+    (1, 'Haakon Haakonssønn', 3),
+    (1, 'Haakon Haakonssønn', 4),
+    (1, 'Haakon Haakonssønn', 5),
+    (1, 'Inga fra Vartejg', 1),
+    (1, 'Inga fra Vartejg', 3),
+    (1, 'Skule Jarl', 1),
+    (1, 'Skule Jarl', 2),
+    (1, 'Skule Jarl', 3),
+    (1, 'Skule Jarl', 4),
+    (1, 'Skule Jarl', 5),
+    (1, 'Fru Ragnhild', 1),
+    (1, 'Fru Ragnhild', 5),
+    (1, 'Margrete', 1),
+    (1, 'Margrete', 2),
+    (1, 'Margrete', 3),
+    (1, 'Margrete', 4),
+    (1, 'Margrete', 5),
+    (1, 'Sigrid/Ingebjørg', 1),
+    (1, 'Sigrid/Ingebjørg', 2),
+    (1, 'Sigrid/Ingebjørg', 5),
+    (1, 'Biskop Nikolas', 1),
+    (1, 'Biskop Nikolas', 2),
+    (1, 'Biskop Nikolas', 3),
+    (1, 'Gregorius Jonssønn', 1),
+    (1, 'Gregorius Jonssønn', 2),
+    (1, 'Gregorius Jonssønn', 3),
+    (1, 'Gregorius Jonssønn', 4),
+    (1, 'Gregorius Jonssønn', 5),
+    (1, 'Paal Flida/Trønder', 1),
+    (1, 'Paal Flida/Trønder', 2),
+    (1, 'Paal Flida/Trønder', 3),
+    (1, 'Paal Flida/Trønder', 4),
+    (1, 'Paal Flida/Trønder', 5),
+    (1, 'Baard Bratte/Trønder', 1),
+    (1, 'Jatgeir Skald/Dagfinn Bonde', 4),
+    (1, 'Peter', 3),
+    (1, 'Peter', 4),
+    (1, 'Peter', 5);
+
+    #Størst av alt er kjærligheten
+INSERT INTO RolleIAkt(`SID`, `RolleNavn`, `AktNr`) VALUES
+    (2,'Sunniva Du Mond Nordal',1),
+    (2,'Jo Sabiernak',1),
+    (2,'Marte M. Steinsholt',1),
+    (2,'Tor Ivar Hagen',1),
+    (2,'Trond-Ove Skrødal',1),
+    (2,'Natalie Grøndahl Tangen',1),
+    (2,'Åsmund Flaten',1);
+
+    
+
+
+
+
