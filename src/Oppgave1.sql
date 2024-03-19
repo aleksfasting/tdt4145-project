0a2dcfec3cf3187b43f1ccdda1eed738255996ba
Ulrik Haugland <ulhaugland@gmail.com>
2024-03-19 11:58:47 +0100

    JAJJJAJJAJJA

--- src/Oppgave1.sql
+++ src/Oppgave1.sql
@@ -164,7 +164,14 @@ INSERT INTO RolleIAkt(`SID`, `RolleNavn`, `AktNr`) VALUES
     (2,'Natalie Grøndahl Tangen',1),
     (2,'Åsmund Flaten',1);
 
-INSERT INTO spillerrolle(`PID`,`SID`,`RolleNavn`)
+INSERT INTO spillerrolle(`PID`,`SID`,`RolleNavn`) VALUES
+    ()
+
+INSERT INTO medvirker(`PID`,`SID`,`Jobb`) VALUES
+    ()
+
+INSERT INTO sete(`Område`,`RadNr`,`SeteNr`) VALUES
+    ()
 
     
 
