b0ecefde7514a00a5c77dcd2011fe0a65fdf90ba
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-03-20 20:35:33 +0100

    forslag til løsning

--- src/brukstilfelle4.py
+++ src/brukstilfelle4.py
@@ -1,11 +1,8 @@
 from connection import con
 import datetime
 
-cursor = con.cursor()
-
-### Funksjon for å sjekke at datoen er gyldig
 def checkDate(dateArg):
-
+    """Sjekker at argumentet er en gyldig dato"""
     ### Sjekker at formatet stemmer
     dateList = dateArg.split('-')
     if (len(dateList) != 3 and len(dateList[0]) != 4 and len(dateList[1]) != 2 and len(dateList[2]) != 2):
@@ -24,26 +21,39 @@ def checkDate(dateArg):
     except (ValueError):
         raise Exception("Value of date is out of range")
 
+def finnForestilling(date):
+    """Finner alle forestillinger på en gitt dato"""
+    cursor.execute(
+    """SELECT SID, Navn, F.Dato, F.Tid, COUNT(KjøpID) AS AntallBilletter
+    FROM    Forestilling AS F LEFT NATURAL JOIN (
+            BillettKjøpForestilling NATURAL JOIN BillettSete)
+            NATURAL JOIN TeaterStykke
+    WHERE F.Dato = ?
+    GROUP BY SID, Navn, F.Dato, F.Tid""",
+    (date, )
+    )
 
-### Spør bruker om gyldig dato
-date = input("Select date: (yyyy-mm-dd) ")
-checkDate(date)
+def printTable(table):
+    """Printer tuplene slik at den ser litt bedre ut"""
+    for tup in table:
+        print(tup)
 
 
+def main():
+    ### Spør bruker om gyldig dato
+    date = input("Select date: (yyyy-mm-dd) ")
+    checkDate(date)
 
-### Finn alle forestillinger på datoen
-### Bruker GROUP BY og COUNT for å finne antall billetter
-### Antall rader for en forestilling blir lik antall billetter siden det finnes 
-###     en 1-1 relasjon fra billett til forestilling gjennom BillettKjøp
-cursor.execute(
-"""SELECT SID, Navn, F.Dato, F.Tid, COUNT(*) AS AntallBilletter
-FROM    Forestilling AS F NATURAL JOIN (
-        BillettKjøpForestilling NATURAL JOIN BillettSete)
-        NATURAL JOIN TeaterStykke
-WHERE F.Dato = ?
-GROUP BY SID, Navn, F.Dato, F.Tid""",
-(date, )
-)
+    global cursor
+    cursor = con.cursor()
 
-result = cursor.fetchall()
-print(result)
+    finnForestilling(date)
+
+    result = cursor.fetchall()
+    printTable(result)
+
+    con.commit()
+    con.close()
+
+if __name__ == "__main__":
+    main()
