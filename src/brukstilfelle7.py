8cdba7aa4681e857d2c10cf6a31ecaf04b84b600
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-03-20 22:34:53 +0100

    forslag til oppgave 7

---
+++ src/brukstilfelle7.py
@@ -0,0 +1,38 @@
+from connection import con
+
+def finnSkuesplillere(navn):
+    """SQL Select for å finne skuesplillere som har split sammen med 'navn'"""
+
+    cursor.fetchall()
+    
+    cursor.execute(
+        """SELECT DISTINCT P1.Navn AS Skuespiller1, P2.Navn AS SKUESPILLER2, RIA1.SID
+        FROM    (((Person AS P1 NATURAL JOIN Skuespiller) NATURAL JOIN SpillerRolle AS SR1)
+                NATURAL JOIN RolleIAkt AS RIA1)
+                INNER JOIN
+                (((Person AS P2 NATURAL JOIN Skuespiller) NATURAL JOIN SpillerRolle AS SR2)
+                NATURAL JOIN RolleIAkt AS RIA2) ON (RIA1.SID = RIA2.SID AND RIA1.AktNr = RIA2.AktNr)
+        WHERE Skuespiller1 = ? AND NOT Skuespiller2 = ?""",
+        (navn, navn)
+        )
+
+    return cursor.fetchall()
+
+def printRelasjon(table): ### For å printe litt finere enn vanlig for lister
+    for row in table:
+        print(row)
+
+def main():
+    navn = input("Skuespiller: ")
+    
+    global cursor
+    cursor = con.cursor()
+
+    result = finnSkuesplillere(navn)
+
+    printRelasjon(result)
+
+    con.close()
+
+if __name__ == '__main__':
+    main()
