fbb2edb0df4b59c8ea3bfd44b11f5ec64f9f2e02
Aleksander Fasting <aleksasf@stud.ntnu.no>
2024-03-21 15:10:57 +0100

    småendringer

--- innlevering2/runner.py
+++ innlevering2/runner.py
@@ -4,35 +4,51 @@ sb.run(["python3", "./connection.py"])
 
 print("\nLaget `teaterDB.db`\n")
 
+print('=============================================================================')
+
 sb.run(["sqlite3", "teaterDB.db", ".read create-tables.sql"])
 
 print("\nLaget tabeller\n")
+
+print('=============================================================================')
 
 print("\nOPPGAVE 1:")
 sb.run(["sqlite3", "teaterDB.db", ".read oppgave1.sql"])
 sb.run(["python3", "./oppgave1.py"])
 print("    Satt inn tupler\n")
 
+print('=============================================================================')
+
 print("\nOPPGAVE 2:")
 sb.run(["python3", "./oppgave2.py"])
 print("    Satt inn kjøp\n")
 
+print('=============================================================================')
+
 print("\nOPPGAVE 3:")
 sb.run(["python3", "./oppgave3.py"])
-print("    Satt inn 9 nye kjøp på ledige rader")
+print("    Satt inn 9 nye kjøp på ledige rader\n")
 
-print("\nOPPGAVE 4:")
+print('=============================================================================')
+
+print("\nOPPGAVE 4:    (input: '2024-02-03')\n")
 sb.run(["python3", "./oppgave4.py"], input="2024-02-03\n", text=True)
 print("\n    Hentet tabell\n")
 
-print("\nOPPGAVE 5:")
+print('=============================================================================')
+
+print("\nOPPGAVE 5:\n")
 sb.run(["sqlite3", "teaterDB.db", ".read oppgave5.sql"])
 print("\n    Hentet tabell\n")
 
-print("\nOPPGAVE 6:")
-sb.run(["sqlite3", "teaterDB.db", ".read oppgave6.sql"])
-print("\n    Hentet tabell")
+print('========================================================================')
 
-print("\nOPPGAVE 7:")
-sb.run(["python3", "./oppgave7.py"], input="Artuto Scotti\n", text=True)
-print("    Hentet tabell\n")
+print("\nOPPGAVE 6:\n")
+sb.run(["sqlite3", "teaterDB.db", ".read oppgave6.sql"])
+print("\n    Hentet tabell\n")
+
+print('========================================================================')
+
+print("\nOPPGAVE 7:    (input: 'Arturo Scotti')\n")
+sb.run(["python3", "./oppgave7.py"], input="Arturo Scotti\n", text=True)
+print("\n    Hentet tabell\n")
